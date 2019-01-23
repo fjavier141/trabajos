9b4b312daeabaf8e9d17cc577e42e1bc3d1cdf68
Francisco Latorre Carvajal <f.latorre.carvajal@gmail.com>
2019-01-23 13:18:06 -0300

    hello

--- FJL&BROTHERS/js/menuInicio.js
+++ FJL&BROTHERS/js/menuInicio.js
@@ -4,4 +4,11 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     item.style.color = '#1c9b0c';
     item.style.textShadow = '1px 1px 2px #1c9b0c';
   }
+
+  $("#navbarToggle").click(function (event) {
+
+      document.getElementById("left").style.display = 'none';
+      document.getElementById("right").style.display = 'none';
+
+  });
 });
