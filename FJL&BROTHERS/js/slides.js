50923efa3484f71ea119a4a023401fe643cd390b
Francisco Latorre Carvajal <f.latorre.carvajal@gmail.com>
2019-01-22 22:30:18 -0300

    hey

--- FJL&BROTHERS/js/slides.js
+++ FJL&BROTHERS/js/slides.js
@@ -1,31 +1,43 @@
-var index = 1;
+var index = 0;
 
 //imageSlide();
+
+function previousSlide(n){
+  index = index + n;
+  manualSlide(index);
+}
+
+function nextSlide(n){
+  index = index + n;
+  manualSlide(index);
+}
 
 imageSlide();
 function imageSlide(){
   var slide = document.getElementsByClassName("slide");
-  if(index > slide.length){
-    index = 1;
+  if(index >= slide.length){
+    index = 0;
   }
   for(var i = 0; i < slide.length; i++){
     slide[i].style.display = "none";
   }
-  slide[index-1].style.display = "block";
+  slide[index].style.display = "block";
   index = index + 1;
   setTimeout(imageSlide, 5000);
+  console.log(index);
+  console.log(slide[index]);
 }
 
 function manualSlide(n){
   var slide = document.getElementsByClassName("slide");
-  if(n > slide.length){
-    index = 1;
+  if(n >= slide.length){
+    index = 0;
   }
-  if(n < 1){
-    index = slide.length;
+  if(n < 0){
+    index = slide.length - 1;
   }
   for(var i = 0; i < slide.length; i++){
     slide[i].style.display = "none";
   }
-  slide[n-1].style.display = "block";
+  slide[index].style.display = "block";
 }
