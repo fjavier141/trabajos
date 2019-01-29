b13329e865deca9a34a5dd53a374e65c46433e12
Francisco Latorre Carvajal <f.latorre.carvajal@gmail.com>
2019-01-29 13:31:43 -0300

    me

--- FJL&BROTHERS/js/menuPortafolio.js
+++ FJL&BROTHERS/js/menuPortafolio.js
@@ -4,14 +4,35 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     item.style.color = '#1c9b0c';
     item.style.textShadow = '1px 1px 2px #1c9b0c';
   }
-  var atacama = document.getElementById('CL-AT');
-  atacama.addEventListener("mouseover", funcion);
-  atacama.addEventListener("mouseover", funcion);  
 
-  function funcion(){
+  $('#CL-AT').mouseover(function(){
     document.getElementById('region-III').style.display = 'block';
-  }
-  setTimeout(function(){
+  })
+  $('#CL-AT').mouseout(function(){
     document.getElementById('region-III').style.display = 'none';
-  },1000)
+  })
+  $('#CL-CO').mouseover(function(){
+    document.getElementById('region-IV').style.display = 'block';
+  })
+  $('#CL-CO').mouseout(function(){
+    document.getElementById('region-IV').style.display = 'none';
+  })
+  $('#CL-RM').mouseover(function(){
+    document.getElementById('region-RM').style.display = 'block';
+  })
+  $('#CL-RM').mouseout(function(){
+    document.getElementById('region-RM').style.display = 'none';
+  })
+  $('#CL-VS').mouseover(function(){
+    document.getElementById('region-V').style.display = 'block';
+  })
+  $('#CL-VS').mouseout(function(){
+    document.getElementById('region-V').style.display = 'none';
+  })
+  $('#CL-BI').mouseover(function(){
+    document.getElementById('region-VIII').style.display = 'block';
+  })
+  $('#CL-BI').mouseout(function(){
+    document.getElementById('region-VIII').style.display = 'none';
+  })
 });
