5d025f7c69a18c13ec4b9f995e9bef5b162a2264
Francisco Latorre Carvajal <f.latorre.carvajal@gmail.com>
2019-01-29 04:12:19 -0300

    me

--- FJL&BROTHERS/js/menuPortafolio.js
+++ FJL&BROTHERS/js/menuPortafolio.js
@@ -4,4 +4,14 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     item.style.color = '#1c9b0c';
     item.style.textShadow = '1px 1px 2px #1c9b0c';
   }
+  var atacama = document.getElementById('CL-AT');
+  atacama.addEventListener("mouseover", funcion);
+  atacama.addEventListener("mouseover", funcion);  
+
+  function funcion(){
+    document.getElementById('region-III').style.display = 'block';
+  }
+  setTimeout(function(){
+    document.getElementById('region-III').style.display = 'none';
+  },1000)
 });
